0c5a962fd087743e08697985cc97818d40514614
Mantra.Mehta <84002990+mantramehtapy@users.noreply.github.com>
2021-07-17 19:54:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+#use this command to install package
+
+#pip3 install streamlit 
+
+#pip3 install pandas
+
+# pip3 install numpy
+
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -11,3 +19,4 @@ expander = st.beta_expander("FAQ")
 expander.text("created by Mantra.Mehta")
 st.write("check out my [discord](https://discord.com/invite/NCyRpbqQJT)")
 st.write("check out my [github](https://github.com/mantramehtapy)")
+st.write("check out my [youtube channel](https://www.youtube.com/channel/UC0TMsiwESD3AjacKiBDc0eQ)
